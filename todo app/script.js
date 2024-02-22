a80d3fefa960f0a5c68944af38a3f8993d2cc223
EricaMarzi <erica.marzi.1993@gmail.com>
2024-02-22 19:15:41 +0100

    test list v-for

--- todo app/script.js
+++ todo app/script.js
@@ -1,5 +1,29 @@
 const { createApp } = Vue;
 const app = createApp({
+    data: () => ({
+        tasks: [
+            {
+                "id": 1,
+                "text": "HTML",
+                "done": false
+            },
+            {
+                "id": 2,
+                "text": "CSS",
+                "done": false
+            },
+            {
+                "id": 3,
+                "text": "Responsive design",
+                "done": false
+            },
+            {
+                "id": 4,
+                "text": "Javascript",
+                "done": false
+            }
+        ]
+    })
 
 });
 
